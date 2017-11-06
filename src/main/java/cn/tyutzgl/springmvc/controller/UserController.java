47265fffd826a3b38f349059de5063ca9e00a147
zgl <947082708@qq.com>
2017-11-06 15:42:02 +0800

    zgl

--- src/main/java/cn/tyutzgl/springmvc/controller/UserController.java
+++ src/main/java/cn/tyutzgl/springmvc/controller/UserController.java
@@ -1,16 +1,11 @@
 package cn.tyutzgl.springmvc.controller;
 
-import cn.tyutzgl.springmvc.pojo.Items;
 import cn.tyutzgl.springmvc.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @Controller
 public class UserController {
